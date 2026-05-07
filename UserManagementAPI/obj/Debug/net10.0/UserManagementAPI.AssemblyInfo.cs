327f01e47f088e533a43a22426ab5e511bb81f79
NguyenHieuDavitDev <hieu102056@donga.edu.vn>
2026-05-08 00:19:09 +0700

    publish release v1.0.1

--- UserManagementAPI/obj/Debug/net10.0/UserManagementAPI.AssemblyInfo.cs
+++ UserManagementAPI/obj/Debug/net10.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516324355316c354af8c6c566b42410d4abd1bd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab77a80e8ffa34d1c3ad46a7e84f12b2f3011b83")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
