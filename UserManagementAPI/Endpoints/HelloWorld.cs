f41606111111cd0e15e360a4c598a51d396ce6d8
NguyenHieuDavitDev <hieu102056@donga.edu.vn>
2026-05-08 00:06:18 +0700

    #7 - feature: update hello world

--- UserManagementAPI/Endpoints/HelloWorld.cs
+++ UserManagementAPI/Endpoints/HelloWorld.cs
@@ -4,6 +4,8 @@ public static class HelloWorld
 {
     public static void MapHelloWorld(this WebApplication app)
     {
-        app.MapGet("/", () => "Hello World from HelloWorld endpoint 1!");
+        app.MapGet("/", () => "Hello World from HelloWorld endpoint! Xin chào các bạn!");
+        app.MapGet("/an", () => "Xin chào");
+        
     }
 }
